f6cd02f64f64fbb3fec6427e527df870a9b9b625
Fritz Geib <fritz@standland.org>
2021-09-04 16:45:30 -0400

    remove stupid commas

--- Casks/qdriverstation.rb
+++ Casks/qdriverstation.rb
@@ -1,7 +1,7 @@
 cask "qdriverstation" do
   version "21.04"
   sha256 "1549157d3d79e68631f38b5af675d2c84bfd1946980cd9aed10f2849ddd74ff6"
-  url "https://github.com/FRC-Utilities/QDriverStation/releases/download/v#{version}/QDriverStation-#{version}-macOS.zip",
+  url "https://github.com/FRC-Utilities/QDriverStation/releases/download/v#{version}/QDriverStation-#{version}-macOS.zip"
   name "qdriverstation"
   desc "a cross-platform and open-source alternative to the FRC Driver Station"
   homepage "https://frc-utilities.github.io/"

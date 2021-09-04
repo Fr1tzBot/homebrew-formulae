24b6b33df93f3236581e31ab2bd2b560e55783c7
Fritz Geib <fritz@standland.org>
2021-09-04 13:39:42 -0400

    attept to use current openssl version

--- chntpw.rb
+++ chntpw.rb
@@ -6,10 +6,10 @@ class Chntpw < Formula
     sha256 "e915f5addc2673317285c6f022c94da7fdee415d9800cd38540a13706706786b"
     head "https://github.com/sidneys/chntpw.git"
 
-    depends_on "fr1tzbot/homebrew-formulae/openssl@1.0"
+    depends_on "openssl"
 
     def install
-        system "make", "OSSLPATH=#{prefix}/opt/openssl@1.0"
+        system "make", "OSSLPATH=#{prefix}/opt/openssl"
         bin.install "chntpw"
     end
 

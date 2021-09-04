3af931de1adc75b9adc081021bf9e21b41c42339
Fritz Geib <fritz@standland.org>
2021-09-04 13:17:02 -0400

    fix dep on chntpw

--- chntpw.rb
+++ chntpw.rb
@@ -6,7 +6,7 @@ class Chntpw < Formula
     sha256 "e915f5addc2673317285c6f022c94da7fdee415d9800cd38540a13706706786b"
     head "https://github.com/sidneys/chntpw.git"
 
-    depends_on "sidneys/homebrew/openssl@1.0"
+    depends_on "fr1tzbot/homebrew-formulae/openssl@1.0"
 
     def install
         system "make", "OSSLPATH=#{prefix}/opt/openssl@1.0"

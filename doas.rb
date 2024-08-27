33f43a0b31e9e37d797d3dc42e57de1e5ed05c66
Fritz Geib <fritz@standland.org>
2024-08-27 09:13:18 -0400

    doas 6.8.2.2

--- doas.rb
+++ doas.rb
@@ -1,8 +1,8 @@
 class Doas < Formula
     desc "A portable fork of the OpenBSD `doas` command"
     homepage "https://github.com/Fr1tzBot/OpenDoas"
-    url "https://github.com/Fr1tzBot/OpenDoas/archive/refs/tags/v6.8.2.tar.gz"
-    sha256 "6da058a0e70b7543bc60624389b0b00b686189ec933828c522bf8b2600495a67"
+    url "https://github.com/Fr1tzBot/OpenDoas/archive/refs/tags/v6.8.2.2.tar.gz"
+    sha256 "3976576c7a47aa20e30613e54cf65f34a38a8cb338d6de6d641151907716a6c6"
     head "https://github.com/Fr1tzBot/OpenDoas.git"
   
   
